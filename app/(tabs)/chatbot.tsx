b930c9df035b7e25e725f1c1fa1ed65218896558
juhyun <hanaheart030@gmail.com>
2025-07-11 20:44:44 +0900

    기본 초기 세팅

---
+++ app/(tabs)/chatbot.tsx
@@ -0,0 +1,10 @@
+// app/chatbot.tsx
+import { View, Text } from 'react-native';
+
+export default function ChatbotScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>🤖 챗봇 화면입니다</Text>
+    </View>
+  );
+}
